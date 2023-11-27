799381bb09fcce961ed123faaf3a395ee99280b8
HelegKumachev <kumachev.oleg@gmail.com>
2023-11-27 21:40:21 +0300

    init commit

---
+++ src/test.js
@@ -0,0 +1,17 @@
+import byField from './game.js';
+
+test('test', () => {
+  const usersCopy = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  const result = byField('health', usersCopy)
+  expect(result).toEqual(expected);
+})
